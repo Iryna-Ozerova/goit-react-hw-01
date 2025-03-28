5ec9bce4c7b9b4eefca728ef6ab396dc86322ad5
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-03-28 23:15:19 +0200

    add task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import userData from "../userData.json";
+
 import Profile from "./Profile/Profile";
 
 export default function App () {
   return (
-    <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-    />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-//import css from "./Profile.module.css";
+import css from "./Profile.module.css";
 
 const Profile = ({
   username,
@@ -9,29 +9,29 @@ const Profile = ({
   stats: { followers, views, likes },
 }) => {
     return (
-      <div>
-  <div>
-    <img
+      <div className={css.card}>
+  <div className={css.description}>
+    <img className={css.avatar}
       src={image}
       alt="User avatar"
     />
-    <p>{username}</p>
-    <p>{tag}</p>
-    <p>{location}</p>
+    <p className={css.userTitle}>{username}</p>
+    <p className={css.userInfo}>{tag}</p>
+    <p className={css.userInfo}>{location}</p>
   </div>
 
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>{followers}</span>
+  <ul className={css.statsList}>
+    <li className={css.statsItem}>
+      <span className={css.statLabel}>Followers</span>
+      <span className={css.statValue}>{followers}</span>
     </li>
-    <li>
-      <span>Views</span>
-      <span>{views}</span>
+    <li className={css.statsItem}>
+      <span className={css.statLabel}>Views</span>
+      <span className={css.statValue}>{views}</span>
     </li>
-    <li>
-      <span>Likes</span>
-      <span>{likes}</span>
+    <li className={css.statsItem}>
+      <span className={css.statLabel}>Likes</span>
+      <span className={css.statValue}>{likes}</span>
     </li>
   </ul>
 </div>
@@ -42,7 +42,7 @@ Profile.propTypes = {
   username: PropTypes.string,
   tag: PropTypes.string,
   location: PropTypes.string,
-  avatar: PropTypes.string,
+  image: PropTypes.string,
   stats: PropTypes.objectOf(PropTypes.number),
 };
 
